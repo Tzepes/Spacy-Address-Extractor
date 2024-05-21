4c16177d9d8d0b999280cd974d1ac3ebb794d6d9
Tyras <andreicristian.vlad@gmail.com>
2024-05-21 15:54:42 +0300

    added logic for organizing organization names and locations for query search

--- routers/location_extraction.py
+++ routers/location_extraction.py
@@ -1,12 +1,17 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
+from typing import Optional
 import spacy
+from utils.similarityScoring import sort_companies_and_locations
+from urllib.parse import urlparse
+
 
 router = APIRouter()
 
 # Define data models for incoming requests
 class TextRequest(BaseModel):
     text: str
+    domain: Optional[str] = None
 
 # Load the pre-trained model for GPE and ORG extraction
 nlp_gpe_org = spacy.load('en_core_web_trf')
@@ -16,8 +21,20 @@ nlp_gpe_org = spacy.load('en_core_web_trf')
 @router.post("/extract_gpe_org/")
 async def extract_gpe_org(request: TextRequest):
     doc = nlp_gpe_org(request.text)
-    gpe = [entity.text for entity in doc.ents if entity.label_ == 'GPE']
-    org = [entity.text for entity in doc.ents if entity.label_ == 'ORG']
-    print(gpe)
-    print(org)
-    return {"GPE": gpe, "ORG": org}
+
+    gpe = []
+    org = []
+    sorted_ORGs_GPEs = []
+
+    for entity in doc.ents:
+        if entity.label_ == 'GPE':
+            gpe.append(entity.text)
+        elif entity.label_ == 'ORG':
+            org.append(entity.text)
+
+    if request.domain:
+        domain = urlparse(request.domain).netloc
+        sorted_ORGs_GPEs = sort_companies_and_locations(domain, org, gpe)
+        return {"GPE": gpe, "ORG": org, "ORG_GPE_Sorted": sorted_ORGs_GPEs}
+    else:
+        return {"GPE": gpe, "ORG": org}
